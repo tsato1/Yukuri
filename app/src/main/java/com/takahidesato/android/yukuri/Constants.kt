9e587ae2b39669f5a432b453650ce84dc9078c8e
tsato1 <a8tb1117@gmail.com>
2021-05-20 00:13:07 +0800

    remove constants file from repo

--- app/src/main/java/com/takahidesato/android/yukuri/Constants.kt
+++
@@ -1,9 +0,0 @@
-package com.takahidesato.android.yukuri
-
-class Constants {
-    companion object {
-        const val BASE_URL = "https://fcm.googleapis.com"
-        const val SERVER_KEY = "AAAAHfGFOL4:APA91bFDR0XRkdeSrHOdddHEtpg95R2PGX58kiMn8DGP1Wz9Vxd1w2LCy8zWIbFV7EoWcxG1aEAJ99rVy4uJvlbG3uKkpnlruiZbH1d0AyIuXIoF49FVq5rsC-luvDpRfP0u-r6wxzHf"
-        const val CONTENT_TYPE = "application/json"
-    }
-}
